78a1f1abe8c62efbcde523cecb72eb1eec3a815e
tklam215 <lam.tsunkit215@gmail.com>
2022-05-11 14:48:03 -0400

    Updated activity file
    declared variables for the app

--- app/src/main/java/edu/temple/weatherapp/MainActivity.kt
+++ app/src/main/java/edu/temple/weatherapp/MainActivity.kt
@@ -1,22 +1,72 @@
 package edu.temple.weatherapp
 
+import android.os.AsyncTask
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import edu.temple.weatherapp.databinding.ActivityMainBinding
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    val CITY: String = "dhaka,bd"
+    val API: String = "add24a5088aed081dd1adb7368be93d2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        weatherTask().execute()
+    }
+    inner class weatherTask(): AsyncTask<String, Void, String>(){
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+            findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
+            findViewById<TextView>(R.id.errorText).visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg p0: String?): String? {
+            var response: String?
+            try{
+                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&appid=$API")
+                    .readText(Charsets.UTF_8)
+            }catch(e: Exception){
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try{
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+                val updatedAt:Long =  jsonObj.getLong("dt")
+                val updatedAtText = "Updated at: " + SimpleDateFormat("dd/MM/yyyy hh:mm a",Locale.ENGLISH).format(Date(updatedAt*1000))
+                val temp = main.getString("temp")+ "°C"
+                val tempMin = "Min Temp: " +main.getString("temp_min")+ "°C"
+                val tempMax = "Max Temp: " +main.getString("temp_max")+ "°C"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+                val sunrise:Long = sys.getLong("sunrise")
+                val sunset:Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+                val address = jsonObj.getString("name") + ", " + sys.getString("country")
+
+                
+            }
+        }
     }
 
 }
